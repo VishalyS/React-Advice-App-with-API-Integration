095c1480da3d54bd0769a5fed7b66ebeb6faf47d
VishalyS <vishalysenthilkumar@gmail.com>
2024-11-09 13:08:09 +0530

    Initial commit

---
+++ ReactAdviceApp/src/AdviceApp.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import "./Advice.css";
+
+export const AdviceApp = () => {
+  const [advice,setAdvice] = useState("Please Click Button to Get Advice");
+  const [count,setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    //console.log(res);
+    const data = await res.json();
+    //console.log(data);
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice(); 
+  },[]);
+
+  return (
+    <div>
+        <h3>{advice}</h3>
+        <button onClick={getAdvice}>Get Advice</button>
+        <Counter count={count} />
+    </div>
+  )
+}
+
+function Counter(props) {
+ return (
+  <p>You have read <b>{props.count}</b> pieces of advice!
+  </p>
+ );
+}
---
+++ ReactAdviceApp/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AdviceApp } from './AdviceApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AdviceApp />
+  </React.StrictMode>
+)
